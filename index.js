1cf1313f7919b517e042bbf377c6e02c052a98be
Jarred FIlmer <jarred.filmer@uqconnect.edu.au>
2016-10-09 23:20:58 +1000

    1

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var fs = require('fs');
+var os = require('os');
 var express = require('express');
 var _ = require('lodash');
 var cron = require('cron');
+var uuid = require('node-uuid');
+
 var app = express();
+
 
 var path = process.env.SILVER_DRIP_FILE_PATH || `${__dirname}/test`;
 
@@ -10,10 +14,11 @@ var rate_per_day = 2;
 
 var items = [];
 function refresh_items () {
-	items = fs.readFileSync(path).toString().split('\n-\n');
+	items = fs.readFileSync(path).toString().split('-');
 }
 refresh_items();
 
+fs.watch(path, refresh_items);
 
 var RSS = require('rss');
 
@@ -23,35 +28,25 @@ var feed = new RSS({
 	site_url: 'http://ec2-52-62-223-45.ap-southeast-2.compute.amazonaws.com:8000'
 });
 
-// new cron.CronJob('* * * * * *', function() {
-// 	var index = _.random(0, items.length);
 
-feed.item({
-	title: 'Silver Drip Item',
-	description: 'test',
-	url: `http://ec2-52-62-223-45.ap-southeast-2.compute.amazonaws.com:8000/0`,
-	// url: `http://ec2-52-62-223-45.ap-southeast-2.compute.amazonaws.com:8000/${index}`,
-	guid:2,
-	date: new Date().toISOString()
-});
+new cron.CronJob('0 7,22 * * *', function() {
+	var index = _.random(0, items.length - 1);
+	var item = items[index].trim();
+	console.log(item)
 
-feed.item({
-	title: 'Silver Drip Item2',
-	description: 'test',
-	url: `http://ec2-52-62-223-45.ap-southeast-2.compute.amazonaws.com:8000/1`,
-	guid: 3,
-	date: new Date().toISOString()
-});
+	var title = (new RegExp(`.*${os.EOL}`, 'gm')).exec(item)[0];
+	item = item.replace(title, '');
 
-feed.item({
-	title: 'Silver Drip Item3',
-	description: 'test',
-	url: `http://ec2-52-62-223-45.ap-southeast-2.compute.amazonaws.com:8000/2`,
-	guid: 4,
-	date: new Date().toISOString()
-});
 
-// }, null, true, 'Australia/Brisbane');
+	feed.item({
+		title: title.trim(),
+		description: item.trim(),
+		url: `http://ec2-52-62-223-45.ap-southeast-2.compute.amazonaws.com:8000/${index}`,
+		guid:uuid.v4(),
+		date: new Date().toISOString()
+	});
+}, null, true, 'America/Los_Angeles', null, true)
+
 
 
 app.all('/', function handler(req, res) {
