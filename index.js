dc04aecc49b2a4ae4a93a28e9de86348b0158862
Jarred FIlmer <jarred.filmer@uqconnect.edu.au>
2016-10-09 22:37:35 +1000

    1

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
+var fs = require('fs');
 var express = require('express');
+var _ = require('lodash');
+var cron = require('cron');
 var app = express();
 
-var path = process.env.SILVER_DRIP_FILE_PATH;
+var path = process.env.SILVER_DRIP_FILE_PATH || `${__dirname}/test`;
 
 var rate_per_day = 2;
+
+var items = [];
+function refresh_items () {
+	items = fs.readFileSync(path).toString().split('\n-\n');
+}
+refresh_items();
+
 
 var RSS = require('rss');
 
 var feed = new RSS({
 	title:'Silver Drip',
 	feed_url: 'ec2-52-62-223-45.ap-southeast-2.compute.amazonaws.com:8000',
-	site_url: 'ec2-52-62-223-45.ap-southeast-2.compute.amazonaws.com:8001'
+	site_url: 'ec2-52-62-223-45.ap-southeast-2.compute.amazonaws.com:8000'
 });
 
-feed.item({
-	title:'Silver Drip Item',
-	description:'test',
-	url:'test.com',
-	guid:1,
-	date: new Date().toISOString()
-});
+new cron.CronJob('* * * * * *', function() {
+	var index = _.random(0, items.length);
 
-app.all('*', function handler(req, res) {
+	feed.item({
+		title: 'Silver Drip Item',
+		description: 'test',
+		url: `http://ec2-52-62-223-45.ap-southeast-2.compute.amazonaws.com:8000/${index}`,
+		guid: Date.now(),
+		date: new Date().toISOString()
+	});
+}, null, true, 'Australia/Brisbane');
+
+
+app.all('/', function handler(req, res) {
 	res.send(feed.xml());
 })
 
-app.listen(8000)
+app.all('/:index', function handler(req, res) {
+	res.send(items[req.params.index]);
+})
+
+app.listen(8000);
